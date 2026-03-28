14382c6973ab3160566a83dfdb932c32c312b34f
Mike Goatly <mikegoatly@hotmail.com>
2026-03-28 10:33:15 +0000

    Add configurable document store and local permissions file

--- src/FirestoreSharp.Server/Program.cs
+++ src/FirestoreSharp.Server/Program.cs
@@ -1,5 +1,6 @@
 using FirestoreSharp.Core;
 using FirestoreSharp.Core.Listeners;
+using FirestoreSharp.Core.Stores.FileSystem;
 using FirestoreSharp.Core.Stores.InMemory;
 using FirestoreSharp.Core.Transactions;
 using FirestoreSharp.Server.Services;
@@ -7,7 +8,6 @@ using FirestoreSharp.Server.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
-builder.Services.AddSingleton<IDocumentStore, InMemoryDocumentStore>();
 builder.Services.AddSingleton<IDocumentService, DocumentService>();
 builder.Services.AddSingleton<ITransactionManager, TransactionManager>();
 builder.Services.AddSingleton<IListenerService, ListenerService>();
@@ -17,9 +17,31 @@ builder.Services.AddSingleton<IListenerService, ListenerService>();
 // to happen on the same instance.
 builder.Services.AddSingleton<IDocumentChangeNotifier>(sp => sp.GetRequiredService<IListenerService>());
 
+var storeArg = args.SkipWhile(a => a != "--store").Skip(1).FirstOrDefault() ?? "InMemory";
+
+if (string.Equals(storeArg, "FileSystem", StringComparison.OrdinalIgnoreCase))
+{
+    var storePath = args.SkipWhile(a => a != "--store-path").Skip(1).FirstOrDefault()
+        ?? Directory.GetCurrentDirectory();
+
+    builder.Services.Configure<FileSystemStorageOptions>(o => o.BasePath = storePath);
+    builder.Services.AddSingleton<IDocumentStore, FileSystemDocumentStore>();
+}
+else if (string.Equals(storeArg, "InMemory", StringComparison.OrdinalIgnoreCase))
+{
+    builder.Services.AddSingleton<IDocumentStore, InMemoryDocumentStore>();
+}
+else
+{
+    Console.Error.WriteLine($"Unknown store type '{storeArg}'. Valid values: InMemory, FileSystem");
+    return 1;
+}
+
 var app = builder.Build();
 
 app.MapGrpcService<FirestoreGrpcService>();
 app.MapGet("/", () => "FirestoreSharp gRPC emulator");
 
 app.Run();
+
+return 0;
